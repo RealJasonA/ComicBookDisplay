7f554473db1b174af6514692f224794b8c4b8a52
Jason <jasonanders@Jasons-iMac.local>
2016-08-16 17:00:23 -0600

    Fixed Issues and re-initialized the Repo

---
+++ ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ComicBookGallery
+{
+	public class Artist
+	{
+		public string Name { get; set; }
+		public string Role { get; set; }
+	}
+}
+
---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComicBookGallery
+{
+	public class ComicBook
+	{
+		public int Id { get; set; }
+		public string SeriesTitle { get; set; }
+		public int IssueNumber { get; set; }
+		public string DescriptionHtml { get; set; }
+		public Artist[] Artists { get; set; }
+		public bool Favorite { get; set; }
+
+		public string DisplayText
+		{
+			get
+			{
+				return SeriesTitle + " #" + IssueNumber;
+			}
+		}
+
+		// series-title-issuenumber.jpg
+		public string CoverImageFileName
+		{
+			get
+			{
+				return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+			}
+		}
+	}
+}
+
